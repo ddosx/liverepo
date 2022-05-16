2c9cc5ae4283f85b935c9cb04e55e9620e9711af
Vova Kolinko <vovakolinko@gmail.com>
2022-05-16 16:25:28 +0300

    update

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 rm -rfv x86_64/liverepo.db x86_64/liverepo.db.tar.gz x86_64/liverepo.files.tar.gz x86_64/liverepo.files
-repo-add x86_64/liverepo.db.tar.gz x86_64/*/*.pkg.tar.zst
+repo-add x86_64/liverepo.db.tar.gz x86_64/*.pkg.tar.zst
 git add .
 git commit -m 'update'
 git push
